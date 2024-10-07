021405bd07f43dfb972a92c040f5b36a34526f49
David-Zeng <david.zengdj@gmail.com>
2024-10-07 22:54:42 +1100

    update the favicon png

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -105,9 +105,7 @@ WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
 
 WEBUI_URL = os.environ.get("WEBUI_URL", "http://localhost:3000")
 
-WEBUI_FAVICON_URL = (
-    "https://drive.google.com/file/d/187FfHCiSFGsIb2mpjB88pR0AhhxZS6I_/view?usp=sharing"
-)
+WEBUI_FAVICON_URL = "https://github.com/David-Zeng/open-webui/blob/cloudU3/static/static/favicon_cloudU3.png"
 
 
 ####################################
